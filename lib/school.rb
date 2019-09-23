a1ea536f62bf41aa00dc9a5e98d48aad17244797
Ashley Norton <nortonashleym@gmail.com>
2019-09-23 00:56:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :name, :roster, :grade
+  attr_reader :school 
+  
+  def initialize(school)
+    @name = name
+    
+  def roster
+    roster = {}
+  end 
+  
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade
+    @roster[:grade] 
+    @roster[:name]
+  end 
+  
+end 
